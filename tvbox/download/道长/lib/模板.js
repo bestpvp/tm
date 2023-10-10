b23bb9617a5907f7e9cd30ee3e5655f6df7792db
bestpvp <bestpvp@sina.com>
2023-10-10 20:49:07 +0800

    清理文件夹

--- tvbox/download/道长/lib/模板.js
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-<body>
-  <h1>公众号：影视范</h1>
-  <p>本接口由公众号：影视范提供免费维护，不定期更新，关注获取最新接口</p>
-  <img src="https://cdn-y.tencentmusic.com/musician/commonPic/cos_71bd048daf8d655f45630e9ae4ec393d019840cf.png" alt="">
-</body>
-</html>
