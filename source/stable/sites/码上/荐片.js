d347c3ee23ed8e90c795ccc9fcdea66278f2c187
bestpvp <bestpvp@sina.com>
2024-03-24 19:10:57 +0800

    update

--- source/stable/sites/码上/荐片.js
+++ source/stable/sites/码上/荐片.js
@@ -13,7 +13,7 @@ var rule = {
     homeUrl: '/api/tag/hand?code=unknown601193cf375db73d&channel=wandoujia',//网站的首页链接,用于分类获取和推荐获取
     // url:'/api/crumb/list?area=0&category_id=fyclass&page=fypage&type=0&limit=24&fyfilter',
     url: '/api/crumb/list?area=0&page=fypage&type=0&limit=24&fyfilter',
-    class_name: '插兜的干货仓库公众号免费提供&电影&电视剧&动漫&综艺',     // 筛选 /api/term/ad_fenlei?limit=10&page=1
+    class_name: '插兜的干货仓库&电影&电视剧&动漫&综艺',     // 筛选 /api/term/ad_fenlei?limit=10&page=1
     class_url: '0&1&2&3&4',
     detailUrl: '/api/node/detail?channel=wandoujia&token=&id=fyid',//二级详情拼接链接(json格式用)
     searchUrl: '/api/video/search?key=**&page=fypage',
